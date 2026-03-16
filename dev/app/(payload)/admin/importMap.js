d80cc72cdf9abf4d55af737e73d5b6471ac4c06a
PascalEugster <61283299+PascalEugster@users.noreply.github.com>
2026-03-16 10:25:03 +0100

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'image-optimizer/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'image-optimizer/rsc'
+
+export const importMap = {
+  'image-optimizer/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'image-optimizer/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
